e3393bffbd9253eea3ef3dfe1f2da6a8f224b370
UpSnow <arthurcel2002@gmail.com>
2026-06-09 19:46:45 -0300

    bug:mudei os valores de int para double

--- src/cofre/compartilhado/Constantes.java
+++ src/cofre/compartilhado/Constantes.java
@@ -5,7 +5,7 @@ public final class Constantes {
 
     public static final int    PORTA             = 12345;
     public static final String HOST_PADRAO       = "localhost";
-    public static final int    CODIGO_MAX        = 4;
+    public static final int    CODIGO_MAX        = 999;
     public static final int    INCREMENTO_FUNDO  = 2;
     public static final double PERCENTUAL_PREMIO = 0.60;
 }
